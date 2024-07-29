60c123228fb1f7433421c5a4a07cf86ab0680adc
Apo <apostu.mihai01@gmail.com>
2024-07-30 01:05:29 +0300

    removed a bad route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { QuizQuestionsComponent } from './quiz-questions/quiz-questions.component';
 export const routes: Routes = [
    
-    {
+       {
         path:"",
-        component: HomeComponent,
-        title: "Quiz App"
-    },
-    {
-        path:"quiz/questions",
         component: QuizQuestionsComponent,
         title: 'Quiz Game'
     }
